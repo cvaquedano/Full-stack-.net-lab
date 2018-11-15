02b2fd9d16ce73450d887a6c5b9125dbadab7ab3
cvaquedano <chvb2002@hotmail.com>
2018-11-15 11:31:16 -0600

    Usign Expresesion to replace magic string

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -57,10 +57,11 @@ namespace GigHub.Controllers
         {
             var viewModel = new GigFormViewModel
             {
-                Genres = _context.Genre.ToList()
+                Genres = _context.Genre.ToList(),
+                Heading= "Add a Gig"
             };
 
-            return View(viewModel);
+            return View("GigForm",viewModel);
         }
 
         [Authorize]
@@ -71,7 +72,7 @@ namespace GigHub.Controllers
             if (!ModelState.IsValid)
             {
                 viewModel.Genres = _context.Genre.ToList();
-                return View("Create", viewModel);
+                return View("GigForm", viewModel);
             }
 
            
@@ -87,6 +88,50 @@ namespace GigHub.Controllers
             _context.SaveChanges();
 
             return RedirectToAction("Mine","Gigs");
+        }
+
+        [Authorize]
+        public ActionResult Edit( int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var gig = _context.Gigs.Single(g => g.Id == id && g.ArtistId == userId);
+
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genre.ToList(),
+                Date = gig.DateTime.ToString("d MMM yyyy"),
+                Time = gig.DateTime.ToString("HH:mm"),
+                Genre = gig.GenreId,
+                Venue = gig.Venue,
+                Heading = "Edit a Gig",
+                id = gig.Id
+            };
+
+            return View("GigForm",viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update(GigFormViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genre.ToList();
+                return View("GigForm", viewModel);
+            }
+
+            var userId = User.Identity.GetUserId();
+            var gig = _context.Gigs.Single(g => g.Id == viewModel.id && g.ArtistId == userId);
+
+            gig.Venue = viewModel.Venue;
+            gig.DateTime = viewModel.GetDateTime();
+            gig.GenreId = viewModel.Genre;
+
+           
+            _context.SaveChanges();
+
+            return RedirectToAction("Mine", "Gigs");
         }
     }
 }
